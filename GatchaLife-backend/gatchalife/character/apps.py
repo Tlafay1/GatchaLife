dda3c97dd328badac206b4ef7920301e8c1b4ddf
Timothée Lafay <tlclash12@gmail.com>
2025-12-27 21:01:45 +0100

    f

--- GatchaLife-backend/gatchalife/character/apps.py
+++ GatchaLife-backend/gatchalife/character/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CharacterConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "gatchalife.character"
+
+    def ready(self):
+        import gatchalife.character.handlers
--- GatchaLife-backend/gatchalife/generated_image/apps.py
+++ GatchaLife-backend/gatchalife/generated_image/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GeneratedImageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gatchalife.generated_image'
+
+    def ready(self):
+        import gatchalife.generated_image.handlers
